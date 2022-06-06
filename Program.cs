0ce1c6680247cf2cd1ee1d306f55cabd2c54671d
Juan Hernandez <jualherac@gmail.com>
2022-06-05 19:57:55 -0500

    Lab 1 initial commit

--- Program.cs
+++ Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+Console.WriteLine("Please enter a number");
+
+int n = int.Parse(Console.ReadLine());
+
+string[] wordList = new string[n];
+
+Console.WriteLine("Please enter {0} words", n);
+
+wordList[0] = (Console.ReadLine());
+wordList[1] = (Console.ReadLine());
+wordList[2] = (Console.ReadLine());
+
+string leter = "";
+
+for (int i = 0; i < wordList.Length; i++)
+{
+    if (wordList[i] == leter)
+}
+
+
+
+
+
+
+
+
+
+
+
