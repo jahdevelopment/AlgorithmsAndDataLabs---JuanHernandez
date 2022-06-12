b538d4a821f223dc941c5d330acbbd8a13184700
Juan Alberto Hernandez <jualherac@gmail.com>
2022-06-12 07:53:36 -0500

    Making the last improvements of the function and including comments to explaing the logic or the program

--- Program.cs
+++ Program.cs
@@ -1,12 +1,14 @@
-﻿int length = GetNumberInput();
-string[]words = PopulateWordArray(length);
+﻿//Making a call of all of the functions consecutively 
+int length = GetNumberInput();
+string[]stringOfWords = PopulateWordArray(length);
 Char charToCount = GetCharacterInput();
+string words = addingWords(stringOfWords);
 int charFrequency = CountCharacters(words, charToCount);
-Console.WriteLine($"frequency: {charFrequency}");
-double charPercentage = GetCharacterOcurrencePercentage(words, charToCount);
-Console.WriteLine($"percentage: {charPercentage}");
+double charPercentage = GetCharacterOcurrencePercentage(words, charFrequency);
 PrintResults(charToCount, charFrequency, charPercentage);
 
+
+//Using the console, we ask the user for the number or words that the array is going to have, this function return the answer 
 int GetNumberInput()
 {
     Console.WriteLine("Please enter a number");
@@ -14,16 +16,18 @@ int GetNumberInput()
 }
 
 
+// With this function we make a request to the user for writting the words depending by the number he said previously.
+// With a foor loop we place each word in a string, and all is going to be stored in an array called WordList wich is te return of this function
 string[] PopulateWordArray(int length)
 {
     string[] wordList = new string[length];
     Console.WriteLine($"please enter {length} words");
     for (int i = 0; i < wordList.Length; i++)
     {
-        string wordinput = Console.ReadLine();
-        if (!wordinput.Any(Char.IsWhiteSpace))
+        string wordInput = Console.ReadLine();
+        if (!wordInput.Any(Char.IsWhiteSpace))
         {
-            wordList[i] = wordinput;
+            wordList[i] = wordInput;
         }
         else
         {
@@ -34,29 +38,40 @@ string[] PopulateWordArray(int length)
 }
 
 
+//Using the console, we ask the user for the character that we are going to search in the array
+//this function return the answer converted to a Lower Case leter in the case the user prompt it in Upper Case
 char GetCharacterInput()
 {
     Console.WriteLine("Please enter a character");
-    return Char.Parse(Console.ReadLine());
+    Char leter = Char.Parse(Console.ReadLine());
+    Char leter1 = char.ToLower(leter);
+    return leter1;
 }
 
 
-int CountCharacters(string[] words, char charToCount)
+//Using a foor loop we iterate into the array when the words are stored, and we make a concatenation or each of them in just one string
+string addingWords(string[] stringOfWords)
 {
-    int totalChars = 0;
-    int countedCharacter = 0;  
     string oneWord = "";
 
-    for (int i = 0; i < words.Length; i++)   
+    for (int i = 0; i < stringOfWords.Length; i++)
     {
-        oneWord = string.Join("", words);
+        oneWord = string.Join("", stringOfWords);
     }
+    string wordInLower = oneWord.ToLower();
+    return wordInLower;
+}
 
-    totalChars += oneWord.Length;
-    
-    for (int i = 0; i < oneWord.Length; i++)  
+
+//With the for loop we iterate in the new string and compare the character given by the user with each leter in the string called words which contains the return of the above function,
+//in the case there are both equals we are adding a unit in the countedCharacter variable, this is the return of ConuntCharacters function
+int CountCharacters(string words, char charToCount)
+{
+    int countedCharacter = 0;  
+            
+    for (int i = 0; i < words.Length; i++)  
     {
-        if (oneWord[i].Equals(charToCount))
+        if (words[i].Equals(charToCount))
         {
             countedCharacter++;
         }
@@ -65,34 +80,26 @@ int CountCharacters(string[] words, char charToCount)
 }
 
 
-double GetCharacterOcurrencePercentage(string[] words, char charToCount)
+// this function brings the return of the CountCharacters function into the chatFrecuency variable,
+// we calculate the number of characters present in the string words with the use of Length property
+// finally we calculate and return the percentage with the use of charFrequency and totalChars
+double GetCharacterOcurrencePercentage(string words,int charFrequency)
 {
     double totalChars = 0;
-    double countedCharacter = 0;
-    string oneWord = "";
+
+    totalChars = words.Length;
     
-
-    for (int i = 0; i < words.Length; i++)
-    {
-        oneWord = string.Join("", words);
-    }
-
-    totalChars += oneWord.Length;
-    
-    for (int i = 0; i < oneWord.Length; i++)
-    {
-        if (oneWord[i].Equals(charToCount))
-        {
-            countedCharacter++;
-        }
-    }
-
-    double percentage = (countedCharacter / totalChars) * 100;
+    double percentage = Math.Round((charFrequency / totalChars) * 100);
 
     return percentage;
 }
 
 
+// Based on the frequency and percenate of appearance of the leter we search in the string,
+// we run different messages, one when the letter apears jus one time,
+// another betweeen 2 and 25% of appearance
+// the thir messsage is in the case the letter is up more than 25% of appearance
+// the lats messaje is in the case the leter was not found in the searching
 void PrintResults(char charToCount, int charFrequency, double charPercentage)
 {
     if (charFrequency == 1)
@@ -101,11 +108,15 @@ void PrintResults(char charToCount, int charFrequency, double charPercentage)
     }
     else if (charFrequency > 1 && charPercentage < 25)
     {
-        Console.WriteLine($"\nThe character {charToCount} appears {charFrequency} times in the array.");
+        Console.WriteLine($"\nThe character {charToCount} appears {charFrequency} times in the array.This is the {charPercentage}% of the total number of characters.");
     }
     else if (charFrequency > 1 && charPercentage >= 25)
     {
-        Console.WriteLine($"\nThe character {charToCount} appears {charFrequency} times in the array. This letter makes up more than {charPercentage}% of the total number of characters.");
+        Console.WriteLine($"\nThe character {charToCount} appears {charFrequency} times in the array. This letter makes up more than 25% of the total number of characters.");
+    }
+    else
+    {
+        Console.WriteLine("the character does not appear in the list ot words that you wrote");
     }
 }
 
