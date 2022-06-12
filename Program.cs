7b8d7e8c96ea09119f7355bbf82f495c1262424c
Juan Alberto Hernandez <jualherac@gmail.com>
2022-06-12 07:59:13 -0500

    Initial commit, start of the palindrome search function

--- Program.cs
+++ Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string testString;
+
+bool IsPalindrome(string testString)
+{
+   return false;
+}
