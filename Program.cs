6637daadfb69059c6504c5f114886dc78de06685
Juan Alberto Hernandez <jualherac@gmail.com>
2022-06-14 19:47:34 -0500

    Initial commit

--- Program.cs
+++ Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿List<int> MaxNumbersInLists(List<List<int>> allLists)
+{
+    return greatestNumbers;
+}
